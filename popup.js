c370364e044499560aa2a38c241331dfecfc5253
walksa <miwanishioka701@gmail.com>
2019-03-29 21:55:26 +0800

    added bracket to case no

--- popup.js
+++ popup.js
@@ -5,7 +5,7 @@ $('#btn-u').onclick = function(){
 	chrome.tabs.getSelected(null,function(tab) {
 	    const strUrl = tab.url
 	    const caseNum = strUrl.match(/questions\/(\d+)\//)[1]
-	    const msgStr = `お問い合わせ番号は${caseNum}でございます。`
+	    const msgStr = `お問い合わせ番号は【${caseNum}】でございます。`
 	    document.getElementById("res").innerHTML = txt_msg +  msgStr
 	    chrome.runtime.sendMessage({
 	    	type: 'copy',
