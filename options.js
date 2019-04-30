5637c6386da1012c27b55e6b97250213f1567f95
mn701 <46086846+mn701@users.noreply.github.com>
2019-04-30 09:52:49 +0800

    [added] pre & post msg to storage.sync

--- options.js
+++ options.js
@@ -1,12 +1,16 @@
 $(function(){
-    chrome.storage.sync.get('agentName',function(option){
+    chrome.storage.sync.get(['agentName', 'preMsg', 'postMsg'],function(option){
         $('#agent-name').val(option.agentName)
+        $('#pre-msg').val(option.preMsg)
+        $('#post-msg').val(option.postMsg)
     })
 
     $('#saveName').click(function(){
         const agentName = $('#agent-name').val()
+        const preMsg = $('#pre-msg').val()
+        const postMsg = $('#post-msg').val()
         if (agentName){
-            chrome.storage.sync.set({'agentName': agentName}, function(){
+            chrome.storage.sync.set({'agentName': agentName, 'preMsg':preMsg, 'postMsg':postMsg}, function(){
                 close()
             })
         }
