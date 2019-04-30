335590fb79db17c81c7771c48aed7ad2f5eee66d
mn701 <46086846+mn701@users.noreply.github.com>
2019-04-30 14:43:10 +0800

    [update] updated options

--- options.js
+++ options.js
@@ -1,18 +1,46 @@
 $(function(){
-    chrome.storage.sync.get(['agentName', 'preMsg', 'postMsg'],function(option){
-        $('#agent-name').val(option.agentName)
-        $('#pre-msg').val(option.preMsg)
-        $('#post-msg').val(option.postMsg)
+	
+    chrome.storage.sync.get(['spiel', 'sla90'],function(option){
+        $('#spiel').val(option.spiel)
+        $('#sla90').val(option.sla90)
     })
+    
+    $('#spiel').keyup(updateSample)
+    $('#sla90').keyup(updateSample)
+    $('#check90').change(updateSample)
 
     $('#saveName').click(function(){
-        const agentName = $('#agent-name').val()
-        const preMsg = $('#pre-msg').val()
-        const postMsg = $('#post-msg').val()
-        if (agentName){
-            chrome.storage.sync.set({'agentName': agentName, 'preMsg':preMsg, 'postMsg':postMsg}, function(){
+        const spiel = $('#spiel').val()
+        const sla90 = $('#sla90').val()
+        if (spiel){
+            chrome.storage.sync.set({'spiel': spiel, 'sla90':sla90}, function(){
                 close()
             })
         }
     })
+    
+    function updateSample(){
+    	const greeting = getGreeting()
+    	let txt_over90 = $('#sla90').val()?  "\n" + $('#sla90').val(): "\nチャットが混み合っており、大変お待たせをしておりますことお詫び申し上げます。"
+    	const sla90 = $('#check90').prop('checked')? txt_over90 : ""
+    	let strMsg = $('#spiel').val()
+			    .replace(/{{cusName}}/g, "山田")
+			    .replace(/{{caseNum}}/g, "123456789012345")
+			    .replace(/{{greeting}}/g, greeting)
+				.replace(/{{sla90}}/g, sla90)
+    	$('#sample').html(strMsg)
+    }
+    
+    
+    function getGreeting(){
+		const today = new Date()
+		const hours = today.getHours()
+		let greeting
+		if(hours<11){
+			greeting = "おはようございます"
+		}else{
+			greeting = "こんにちは"
+		}
+		return greeting
+	}
 })
